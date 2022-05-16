58e49b5434be70ec84352c1f49eb86c22715b60e
pany <939630029@qq.com>
2022-05-16 19:01:58 +0800

    fix: 修改 notifcation_title

--- vpnLib/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ vpnLib/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -319,7 +319,7 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
             priority = PRIORITY_DEFAULT;
 
         if (mProfile != null)
-            nbuilder.setContentTitle(getString(R.string.notifcation_title, mProfile.mName));
+            nbuilder.setContentTitle(getString(R.string.notifcation_title));
         else
             nbuilder.setContentTitle(getString(R.string.notifcation_title_notconnect));
 
@@ -647,7 +647,7 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
             }).start();
 
         }else{
-            Log.d("VPNex" , "null timeout");
+            Log.d("UN-VPN" , "null timeout");
         }
 
 
